453090038e89388e870c12b0bf9e87d1098b330c
Anson Zeall <ubuntu@ip-10-212-115-78.ec2.internal>
2013-07-15 04:22:51 +0000

    styled index.html

--- test/grader.js
+++ test/grader.js
@@ -22,20 +22,16 @@ References:
 */
 
 var fs = require('fs');
-var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var TMPHTMLFILE_DEFAULT = "tmp.html";
-var URL_DEFAULT = "http://dry-wave-7782.herokuapp.com/";
-
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); 
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -59,25 +55,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var writeURLToTmp = function(url) {
-    rest.get(url).on('complete', function(result) {
-	fs.writeFileSync("tmp.html",result);
-    });
-};
-
 var clone = function(fn) {
-   return fn.bind({});
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
 };
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_file>', 'Path to url', TMPHTMLFILE_DEFAULT)  
-	.parse(process.argv);
-    
-    writeURLToTmp(URL_DEFAULT);
-
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
